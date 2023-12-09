d01608f999a0989abb848169b6125100c7cdc39f
monali8370 <monalishinde952@gmail.com>
2023-12-09 22:45:57 +0530

    Caculated:Employee Daily Wage

--- src/com/employeewage/Day3_EmployeeWage.java
+++ src/com/employeewage/Day3_EmployeeWage.java
@@ -1,14 +1,21 @@
 package com.employeewage;
 
 public class Day3_EmployeeWage {
-    public static void main(String[] args) {
-        int IS_FULL_TIME=1;
-        double empCheck=Math.floor(Math.random()*10)%2;
-        if (empCheck==IS_FULL_TIME){
-            System.out.println("Employee is Present");
-        }
-        else {
-            System.out.println("Employee is Absent");
-        }
+            static final int WAGE_PER_HOUR = 20;
+            static final int FULL_DAY_HOUR = 8;
+            public static void main(String[] args) {
+                System.out.println("Welcome to employee wage`");
+                int employeeCheck = (int) Math.floor(Math.random()*10)%2;
+                System.out.println(employeeCheck);
+                int dailyWage = 0;
+                if (employeeCheck == 1){
+                    dailyWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
+                    System.out.println("Employee is present");
+                }else {
+                    System.out.println("Employee is Absent");
+                }
+                System.out.println(" Employee daily wage is "+dailyWage);
+            }
     }
-}
+    
+
